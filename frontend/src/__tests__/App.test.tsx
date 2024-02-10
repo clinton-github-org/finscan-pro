ea5a5172b603dc81ea31e909e769b2519732fdc4
clinton1719 <clintoncleetus17@gmail.com>
2024-02-10 09:50:39 +0530

    01 - Test done

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,4 +1,7 @@
+/** @jest-environment jsdom */
 import { cleanup, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import App from '../App';
 
 afterEach(() => {
