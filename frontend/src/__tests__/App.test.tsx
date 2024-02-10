7d87436a84e77d360179f5a492d298511ae92bc7
clinton1719 <clintoncleetus17@gmail.com>
2024-02-10 09:19:00 +0530

    01- Adding tests

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,9 +1,48 @@
-import { render } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders without crashing', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello, World/i);
-  expect(linkElement).toBe(true);
-//   expect(linkElement).toBeInTheDocument();
+describe('renders App component', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('renders main site', () => {
+    const textElement = screen.getByRole('main');
+    expect(textElement).toBeInTheDocument();
+  });
+
+  it('renders heading logo', () => {
+    const headingLogo = screen.getByRole('heading', { name: /FinScan Pro/i });
+    expect(headingLogo).toBeInTheDocument();
+  });
+});
+
+describe('renders HomePage when page is "home" by default', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('renders homepage by default', () => {
+    const textElement = screen.getByText(
+      'Upload your bank statement for analysis!'
+    );
+    expect(textElement).toBeInTheDocument();
+  });
+
+  it('renders form', () => {
+    const formElement = screen.getByTestId('uploadForm');
+    expect(formElement).toBeInTheDocument();
+  });
+
+  // TODO Rewrite after creating results page
+  it('should not render results page', () => {
+    const resultsPage = screen.getByText('Results Page');
+    expect(resultsPage).not.toBeInTheDocument();
+  })
 });
--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -4,17 +4,18 @@ interface HomePage {
   setPage: Dispatch<SetStateAction<string>>;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const HomePage = ({ setPage }: HomePage) => {
   return (
     <>
       <h4>Upload your bank statement for analysis!</h4>
-      <form action="" method="post">
+      <form method="post" data-testid="uploadForm">
         <label htmlFor="fileUpload" id="fileUploadLabel">
           <span>Drop files here</span>
           or
           <input type="file" name="fileUpload" id="fileUpload" accept=".pdf" />
         </label>
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={() => setPage('home')}>Submit</button>
       </form>
     </>
   );
