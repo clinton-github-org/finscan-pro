41d15ac09b130a601fd93b9289b341d4c7fac7ed
clinton1719 <clintoncleetus17@gmail.com>
2024-02-11 17:59:24 +0530

    02 - Adding Lambda

---
+++ lambdas/requestLambda/src/main/java/com/finscanpro/requestLambda/RequestLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.finscanpro.requestLambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestLambdaApplication.class, args);
+	}
+
+}
