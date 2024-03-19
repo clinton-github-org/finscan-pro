b163e7449af80d26440342098f45065421056af5
clinton1719 <clintoncleetus17@gmail.com>
2024-03-19 22:21:05 +0530

    feat(04): Testing with octet

--- frontend/src/services/s3Service.ts
+++ frontend/src/services/s3Service.ts
@@ -8,9 +8,7 @@ interface uploadFileToS3Props {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const uploadFileToS3 = async ({ s3URL, file }: uploadFileToS3Props): Promise<AxiosResponse<any, any>> => {
     try {
-        const response = await axios.put(s3URL, {
-            data: file
-        });
+        const response = await axios.put(s3URL, file);
         return response;
     } catch (error) {
         const axiosError = error as AxiosError;
