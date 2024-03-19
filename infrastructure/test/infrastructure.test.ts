77c4cc47539f8c7d99f2b51cd3a5f709ad275d72
clinton1719 <clintoncleetus17@gmail.com>
2024-03-19 11:01:12 +0530

    feat(04): Fixing deploy pipeline

--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -1,6 +1,7 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
 // import * as Infrastructure from '../lib/infrastructure-stack';
+import '@types/jest';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/infrastructure-stack.ts
