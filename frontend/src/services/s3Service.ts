f19ec2d20016d4666c1e4d6c6d970ee069800030
clinton1719 <clintoncleetus17@gmail.com>
2024-03-19 20:30:53 +0530

    feat(04): Changing content type

--- frontend/src/services/s3Service.ts
+++ frontend/src/services/s3Service.ts
@@ -4,21 +4,22 @@ interface uploadFileToS3Props {
     s3URL: string;
     file: File
 }
-
+axios.defaults.headers.put['Content-Type'] = 'application/octet-stream'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const uploadFileToS3 = async ( {s3URL, file}: uploadFileToS3Props ): Promise<AxiosResponse<any, any>> => {
-   try {
-    const response =  await axios.put(s3URL, {
-        data: file
-       }, {
-        headers: {
-            'Content-Type': 'application/pdf'
-          }
-       });
-       return response;
-   } catch (error) {
-    const axiosError = error as AxiosError;
+const uploadFileToS3 = async ({ s3URL, file }: uploadFileToS3Props): Promise<AxiosResponse<any, any>> => {
+    try {
+        const response = await axios.put(s3URL, {
+            data: file
+        }, {
+            transformRequest: [(data, headers) => {
+                headers["Content-Type"] = 'application/octet-stream';
+                return data;
+            }]
+        });
+        return response;
+    } catch (error) {
+        const axiosError = error as AxiosError;
         throw axiosError.response ? axiosError.response.data : axiosError.message;
-   }
+    }
 }
 export default uploadFileToS3;
